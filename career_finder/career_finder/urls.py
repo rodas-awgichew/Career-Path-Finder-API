109730b34dc323cd1fc81b9ac829ca7af63e75b7
Sadorra <rodiawg@gmail.com>
2025-12-19 16:07:55 +0300

    feat: created path for the default home page

--- career_finder/career_finder/urls.py
+++ career_finder/career_finder/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("<h1>Welcome to Career Finder</h1><p>Try going to <a href='/api/auth/register/'>/api/auth/register/</a></p>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('API.urls')),
+    path('', home_view),
 ]
