0fa8c03028202385f2256f138d68faf3c788f3c0
Sadorra <rodiawg@gmail.com>
2026-02-02 23:17:37 +0300

    feat: Add a catch-all view to serve index.html

--- career_finder/career_finder/urls.py
+++ career_finder/career_finder/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from django.http import HttpResponse
 
 def home_view(request):
@@ -9,4 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('API.urls')),
     path('', home_view),
+        re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='frontend'),
 ]
